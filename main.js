c605fae332cb941fae750f50436121518cba2cdf
Cedar <alexianfireflies@gmail.com>
2019-09-14 12:11:23 -0700

    things

--- main.js
+++ main.js
@@ -1,74 +1,34 @@
-// anime ({
-//     targets: "div.box.red",
-//     translateY: [
-//         {value: 200, duration: 500 },
-//         {value: 0, duration: 500 }
-//     ],
-//     rotate: {
-//         value: "1turn",
-//         easing: "easeInOutSine"
-//     }
-// });
-​
-// anime ({
-//     targets: "div.box.blue",
-//     translateY: [
-//         {value: 200, duration: 500, delay: 1000 },
-//         {value: 0, duration: 500 }
-//     ],
-//     rotate: {
-//         value: "1turn",
-//         easing: "easeInOutSine",
-//         delay: 1000
-//     }
-// });
-​
-// anime ({
-//     targets: "div.box.green",
-//     translateY: [
-//         {value: 200, duration: 500, delay: 2000 },
-//         {value: 0, duration: 500 }
-//     ],
-//     rotate: {
-//         value: "1turn",
-//         easing: "easeInOutSine",
-//         delay: 2000
-//     }
-// });
-​
-// anime ({
-//     targets: "div.box.yellow",
-//     translateY: [
-//         {value: 200, duration: 500, delay: 3000 },
-//         {value: 0, duration: 500 }
-//     ],
-//     rotate: {
-//         value: "1turn",
-//         easing: "easeInOutSine",
-//         delay: 3000
-//     }
-// });
-​
-var boxes = anime ({
-    targets: "div.box",
-    translateY: [
-        {value: 200, duration: 500 },
-        {value: 0, duration: 500 }
-    ],
-    rotate: {
-        value: "1turn",
-        easing: "easeInOutSine"
-    },
-    backgroundColor: "#000",
-    opacity: "0.3",
-    //delay function  takes in the element, the iteration, and the total targets
-    delay: function(element, iteration, totalTargets){return iteration * 1000}, 
-    //won't automatically run on page reload
-    autoplay: false,
-    loop: true
-});
-​
-document.querySelector('#boxes .play').onclick = boxes.play;
-​
-​
-document.querySelector('#boxes .pause').onclick = boxes.pause;
+var nounURL = "https://wordsapiv1.p.mashape.com/words/?random=true&partOfSpeech=noun";
+var adjURL = "https://wordsapiv1.p.mashape.com/words/?random=true&partOfSpeech=adjective";
+var adj;
+var noun;
+
+$.ajax({
+    "async": true,
+	"crossDomain": true,
+    url: adjURL,
+    method: "GET",
+    "headers": {
+		"x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+		"x-rapidapi-key": "e142a905d6mshb8000184dc70a74p1e82aajsnaa2f7a5de3be"
+	}
+}) .then(function(response){
+    console.log(response);
+    adj = response.word;
+    console.log(adj);
+})
+
+$.ajax({
+    "async": true,
+	"crossDomain": true,
+    url: nounURL,
+    method: "GET",
+    "headers": {
+		"x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+		"x-rapidapi-key": "e142a905d6mshb8000184dc70a74p1e82aajsnaa2f7a5de3be"
+	}
+}) .then(function(response){
+    console.log(response);
+    noun = response.word;
+    console.log(noun);
+})
