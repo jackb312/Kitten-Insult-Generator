3b3060c17211beaecd1e66d92198ac1e7c8b9613
Cedar <alexianfireflies@gmail.com>
2019-09-16 21:03:50 -0700

    spacing and on click event in main.js

--- main.js
+++ main.js
@@ -7,38 +7,40 @@ var adjURL = "https://wordsapiv1.p.mashape.com/words/?random=true&partOfSpeech=a
 var adj;
 var noun;
 
-// get adjective, save in adj variable
-$.ajax({
-    "async": true,
-	"crossDomain": true,
-    url: adjURL,
-    method: "GET",
-    "headers": {
-		"x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-		"x-rapidapi-key": "e142a905d6mshb8000184dc70a74p1e82aajsnaa2f7a5de3be"
-	}
-}) .then(function(response){
-    console.log(response);
-    adj = response.word;
-    console.log(adj);
-})
+$("#generate").on("click", function(){
+  // get adjective, save in adj variable
+  $.ajax({
+  "async": true,
+  "crossDomain": true,
+  url: adjURL,
+  method: "GET",
+  "headers": {
+    "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+    "x-rapidapi-key": "e142a905d6mshb8000184dc70a74p1e82aajsnaa2f7a5de3be"
+  }
+  }) .then(function(response){
+  console.log(response);
+  adj = response.word;
+  console.log(adj);
+  })
 
-// get noun, save in noun variable
-$.ajax({
-    "async": true,
-	"crossDomain": true,
-    url: nounURL,
-    method: "GET",
-    "headers": {
-		"x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-		"x-rapidapi-key": "e142a905d6mshb8000184dc70a74p1e82aajsnaa2f7a5de3be"
-	}
-}) .then(function(response){
-    console.log(response);
-    noun = response.word;
-    console.log(noun);
-})
+  // get noun, save in noun variable
+  $.ajax({
+  "async": true,
+  "crossDomain": true,
+  url: nounURL,
+  method: "GET",
+  "headers": {
+    "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+    "x-rapidapi-key": "e142a905d6mshb8000184dc70a74p1e82aajsnaa2f7a5de3be"
+  }
+  }) .then(function(response){
+  console.log(response);
+  noun = response.word;
+  console.log(noun);
+  })
 
-// append
-$("#adjective-2").append(adj);
-$("#noun").append(noun);
+  // append
+  $("#adjective-2").append(adj);
+  $("#noun").append(noun);
+});
