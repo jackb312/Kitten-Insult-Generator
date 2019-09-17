8ae902456ede6b62aa3c02dec48a2e977f6c4de3
Ohmydearcheesus <ohmydearcheesus@gmail.com>
2019-09-16 19:34:37 -0700

    random kitten image generator created

---
+++ random-kitten.js
@@ -0,0 +1,16 @@
+$("#generate").on("click", function() {
+  var settings = {
+    url: "https://api.thecatapi.com/v1/images/search?format=json",
+    method: "GET",
+    headers: {
+      "x-api-key": "17d94b92-754f-46eb-99a0-65be65b5d18f"
+    }
+  };
+  $.ajax(settings).done(function(response) {
+    var image = response[0].url;
+    var element = $("<img></img>");
+    element.attr("src", image);
+    element.addClass("rounded");
+    $("#placeKittenImg").html(element);
+  });
+});
